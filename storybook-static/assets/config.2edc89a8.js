7d7c57a61375a1d2821deb7b801ed5b32c8407eb
Paul Henrot <paul.henrot@idweaver.com>
2022-10-11 03:09:31 +0200

    first commit

---
+++ storybook-static/assets/config.2edc89a8.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.8b5b9a1f.js";import"./iframe.5f57daa8.js";import"./vue.esm-bundler.41fea4e1.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.2edc89a8.js.map
